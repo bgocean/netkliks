f431a3be8766269a8ebab323a3981f5ed28660b3
bgocean <bigocean700@gmail.com>
2024-09-28 12:09:32 +0300

    add js

---
+++ script.js
@@ -0,0 +1,91 @@
+const cardWrapper = document.querySelector(".content-cards");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+const films = [
+  {
+    id: 0,
+    title: "Марсианин 1",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    img: "./images/film.png",
+    link: "./film.html",
+    rating: "7.8",
+  },
+  {
+    id: 1,
+    title: "Марсианин 2",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    img: "./images/film.png",
+    link: "./film.html",
+    rating: "7.8",
+  },
+  {
+    id: 2,
+    title: "Марсианин 3",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    img: "./images/film.png",
+    link: "./film.html",
+    rating: "7.8",
+  },
+  {
+    id: 3,
+    title: "Марсианин 4",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    img: "./images/film.png",
+    link: "./film.html",
+    rating: "7.8",
+  },
+  {
+    id: 4,
+    title: "Марсианин 5",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    img: "./images/film.png",
+    link: "./film.html",
+    rating: "7.8",
+  },
+  {
+    id: 5,
+    title: "Марсианин 6",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    img: "./images/film.png",
+    link: "./film.html",
+    rating: "7.8",
+  },
+];
+
+const render = (array) => {
+  cardWrapper.innerHTML = "";
+
+  array.forEach((item) => {
+    cardWrapper.insertAdjacentHTML(
+      "beforeend",
+      `
+        <a class="content-cards__item" href="/film.html" href="${item.link}">
+          <div class="content-cards__item--img">
+            <img src="${item.img}" alt="film" />
+          </div>
+          <div class="content-cards__item--title">
+            <h5>${item.title},</h5>
+            <span>${item.original}</span>
+          </div>
+          <p class="content-cards__item--description">
+            ${item.category}
+          </p>
+          <p class="content-cards__item--rating">${item.rating}</p>
+        </a>
+      `
+    );
+  });
+};
+
+searchButton.addEventListener("click", () => {
+  render(films.filter((item) => item.title.includes(searchInput.value)));
+});
+
+render(films);
